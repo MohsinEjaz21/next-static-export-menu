d58ca993945105d50850752d7016e6c459b94a25
Mohsin Ejaz <mejaz.bese21@gmail.com>
2022-10-13 17:31:22 +0500

    working on header

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Header from '../Header'
+import Head from 'next/head';
+import Script from 'next/script';
 
 function Layout({ children }) {
   return (
     <>
+      <Script src="/scripts/typetura.min.js"/>
       <Header />
       <main>
         {children}

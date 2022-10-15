0329a0a168e09d8b9eb1bc8a801821e183cdbe16
Mohsin Ejaz <mejaz.bese21@gmail.com>
2022-10-15 19:25:21 +0500

    still working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import React from 'react'
 import Home from '../components/Home'
 import Layout from '../components/Layout'
@@ -5,7 +6,9 @@ import Layout from '../components/Layout'
 function index() {
   return (
     <>
-
+    <Head>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <Layout>
       <Home/>
     </Layout>

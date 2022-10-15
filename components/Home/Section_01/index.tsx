e2bb888fc65c91773ffb6963c9248bc2b5326986
Mohsin Ejaz <mejaz.bese21@gmail.com>
2022-10-15 21:03:08 +0500

    working

--- components/Home/Section_01/index.tsx
+++ components/Home/Section_01/index.tsx
@@ -8,12 +8,12 @@ function Section_01() {
     <section className="section_01">
       <h1 className="h-medium">Centre for <span>Aerospace</span> & <br />  <span>Security</span> Studies</h1>
       <p className="paragraph">An independent analysis on Aerospace and International Security Issues</p>
-      <BtnPlay text={'About CASS'} handleClick={() => window.open("https://www.youtube.com/channel/UCtJntJfsIcBLC9jJdqtA-kQ", '_blank')} />
+        <BtnPlay text={'About CASS'} handleClick={() => window.open("https://youtu.be/2JzP4MGeqDQ", '_blank')} />
       <div className="btn-group">
-        <BtnDefault text={'About CASS'} handleClick={() => window.open("https://youtu.be/2JzP4MGeqDQ", '_blank')} />
-        <img src="https://casstt.com/theme/site/img/utube3.png"  />
+        <BtnPrimary text={'About CASS'} handleClick={() => window.open("https://www.youtube.com/channel/UCtJntJfsIcBLC9jJdqtA-kQ", '_blank')} />
+        {/* <BtnDefault text={'About CASS'} handleClick={() => window.open("https://youtu.be/2JzP4MGeqDQ", '_blank')} /> */}
+        <img src="https://casstt.com/theme/site/img/utube3.png" />
       </div>
-      <BtnPrimary/>
 
     </section>
   )
--- components/widgets/Buttons/BtnPrimary/index.tsx
+++ components/widgets/Buttons/BtnPrimary/index.tsx
@@ -4,12 +4,12 @@ function BtnPrimary(props) {
   return (
     <div className="btn_primary">
       <div className="btn btn-normal">
-        <a href="mailto:info@dennissnellenberg.com" className="btn-click magnetic" data-strength={25} data-strength-text={15} style={{ transform: 'rotate(0.001deg)' }}>
+        <button onClick={props.handleClick} className="btn-click magnetic" data-strength={25} data-strength-text={15} style={{ transform: 'rotate(0.001deg)' }}>
           <div className="btn-fill" style={{ transform: 'translate(0px, -76%)' }} />
           <span className="btn-text" style={{ transform: 'rotate(0.001deg)' }}>
-            <span className="btn-text-inner change" style={{ color: 'rgb(28, 29, 32)' }}>info@dennissnellenberg.com</span>
+            <span className="btn-text-inner change" >{props.text}</span>
           </span>
-        </a>
+        </button>
       </div>
     </div>
 
